1a1f761e507307a485f84188ebda7b8764d17f76
ragini <ragini.venkataraman@gmail.com>
2013-08-20 20:20:32 -0500

    Added more test cases and tie test case

--- exercises/src/PokerHandsTest.java
+++ exercises/src/PokerHandsTest.java
@@ -33,19 +33,63 @@ public class PokerHandsTest {
 		
 		List<PokerHands.Card> blackCards = new ArrayList<>();
 		blackCards.add(new PokerHands.Card("AD"));
-		blackCards.add(new PokerHands.Card("TD"));
-		blackCards.add(new PokerHands.Card("9S"));
-		blackCards.add(new PokerHands.Card("5C"));
-		blackCards.add(new PokerHands.Card("4C"));
+		blackCards.add(new PokerHands.Card("2D"));
+		blackCards.add(new PokerHands.Card("3D"));
+		blackCards.add(new PokerHands.Card("4D"));
+		blackCards.add(new PokerHands.Card("6D"));
 	
 		List<PokerHands.Card> whiteCards = new ArrayList<>();
 		whiteCards.add(new PokerHands.Card("AC"));
-		whiteCards.add(new PokerHands.Card("QC"));
-		whiteCards.add(new PokerHands.Card("7D"));
-		whiteCards.add(new PokerHands.Card("5H"));
 		whiteCards.add(new PokerHands.Card("2C"));
+		whiteCards.add(new PokerHands.Card("3C"));
+		whiteCards.add(new PokerHands.Card("4C"));
+		whiteCards.add(new PokerHands.Card("5C"));
+		
+		Assert.assertEquals("Black wins", new PokerHands().getWinner(blackCards, whiteCards));
+
+	}
+	
+	@Test
+	public void testHighCardCaseThree() {
+		
+		List<PokerHands.Card> blackCards = new ArrayList<>();
+		blackCards.add(new PokerHands.Card("2D"));
+		blackCards.add(new PokerHands.Card("3D"));
+		blackCards.add(new PokerHands.Card("4D"));
+		blackCards.add(new PokerHands.Card("5D"));
+		blackCards.add(new PokerHands.Card("7D"));
+		
+		
+		List<PokerHands.Card> whiteCards = new ArrayList<>();
+		whiteCards.add(new PokerHands.Card("2C"));
+		whiteCards.add(new PokerHands.Card("3C"));
+		whiteCards.add(new PokerHands.Card("4C"));
+		whiteCards.add(new PokerHands.Card("6C"));
+		whiteCards.add(new PokerHands.Card("7C"));
 		
 		Assert.assertEquals("White wins", new PokerHands().getWinner(blackCards, whiteCards));
+
+	}
+	
+	@Test
+	public void testHighCardCaseTie() {
+		
+		List<PokerHands.Card> blackCards = new ArrayList<>();
+		blackCards.add(new PokerHands.Card("2D"));
+		blackCards.add(new PokerHands.Card("3D"));
+		blackCards.add(new PokerHands.Card("4D"));
+		blackCards.add(new PokerHands.Card("5D"));
+		blackCards.add(new PokerHands.Card("6D"));
+		
+		
+		List<PokerHands.Card> whiteCards = new ArrayList<>();
+		whiteCards.add(new PokerHands.Card("2C"));
+		whiteCards.add(new PokerHands.Card("3C"));
+		whiteCards.add(new PokerHands.Card("4C"));
+		whiteCards.add(new PokerHands.Card("5C"));
+		whiteCards.add(new PokerHands.Card("6C"));
+		
+		Assert.assertEquals("Tie", new PokerHands().getWinner(blackCards, whiteCards));
 
 	}
 }
